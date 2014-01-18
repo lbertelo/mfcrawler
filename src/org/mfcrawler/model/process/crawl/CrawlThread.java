bda9ecf7de7090aa74ce72ec85eca3fc43076329
lbertelo <lbertelo@mail.fr>
2014-01-18 15:45:28 +0100

    Improves Logging and Adds a second checking for link

--- src/org/mfcrawler/model/process/crawl/CrawlThread.java
+++ src/org/mfcrawler/model/process/crawl/CrawlThread.java
@@ -1,6 +1,6 @@
 /*
     Mini Focused Crawler : focused web crawler with a simple GUI
-    Copyright (C) 2013  lbertelo
+    Copyright (C) 2013-2014  lbertelo
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
